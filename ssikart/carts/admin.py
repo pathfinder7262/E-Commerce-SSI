c70311bc95ccd2cd93d4d1df8586a3dc5ddf2302
chetan <chetanchaudhari0398@gmail.com>
2022-05-28 00:22:46 +0530

    create models for cart

--- ssikart/carts/admin.py
+++ ssikart/carts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Cart, CartItem
+
+
+
+
+admin.site.register(Cart)
+admin.site.register(CartItem)
--- ssikart/carts/models.py
+++ ssikart/carts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from store.models import Product
+
+
+class Cart(models.Model):
+    cart_id = models.CharField(max_length=250, blank=True)
+    date_added = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.cart_id
+
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.product.product_name
