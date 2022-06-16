ebcbf3dbf11e645d2f919b8a93a547fcc1eff312
chetan <chetanchaudhari0398@gmail.com>
2022-06-16 22:11:26 +0530

    create Variation views for color and size create variation models in store/models.py registered this model in store/admin.py

--- ssikart/store/admin.py
+++ ssikart/store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from store.models import Product
+from store.models import Product,Variation
 # Register your models here.
 
 class Product_admin(admin.ModelAdmin):
@@ -8,4 +8,12 @@ class Product_admin(admin.ModelAdmin):
     }
     list_display = ['product_name','price','stock','is_available','category']
 
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ('product','variation_category','variation_value','is_active')
+    list_editable = ('is_active',)
+    list_filter = ('product' , 'variation_category' , 'variation_value')
+
+
+
 admin.site.register(Product,Product_admin)
+admin.site.register(Variation,VariationAdmin)
